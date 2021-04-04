5b3a9af5c7aafa9cb81252d58e83770d32216a5c
bzoltan1 <zoltan@bakter.hu>
2021-04-04 09:03:38 +0300

    Magic with go deps

--- tgb.go
+++ tgb.go
@@ -17,35 +17,34 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "encoding/json"
-    "net/http"
-    "github.com/gorilla/mux"
-    "io/ioutil"
-    "github.com/go-telegram-bot-api/telegram-bot-api"
-    "gopkg.in/yaml.v2"
-
+	"encoding/json"
+	"fmt"
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"github.com/gorilla/mux"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 type Message struct {
-    Sender   string `json:"Sender"`
-    Content string `json:"content"`
+	Sender  string `json:"Sender"`
+	Content string `json:"content"`
 }
 
 type Configuration struct {
-    Telegram Telegram
-    Server  Server
+	Telegram Telegram
+	Server   Server
 }
 
 type Telegram struct {
-    ChatID  int64
-    Token   string
+	ChatID int64
+	Token  string
 }
 
 type Server struct {
-    Host   string
-    Port   int
+	Host string
+	Port int
 }
 
 var bot *tgbotapi.BotAPI
@@ -53,41 +52,40 @@ var err error
 var c Configuration
 
 func (c *Configuration) getConfiguration() *Configuration {
-    yamlFile, err := ioutil.ReadFile("tgb.yaml")
-    if err != nil {
-        log.Printf("yamlFile.Get err   #%v ", err)
-    }
-    err = yaml.Unmarshal(yamlFile, c)
-    if err != nil {
-        log.Fatalf("Unmarshal: %v", err)
-    }
-    return c
+	yamlFile, err := ioutil.ReadFile("tgb.yaml")
+	if err != nil {
+		log.Printf("yamlFile.Get err   #%v ", err)
+	}
+	err = yaml.Unmarshal(yamlFile, c)
+	if err != nil {
+		log.Fatalf("Unmarshal: %v", err)
+	}
+	return c
 }
-
 
 func sendMessage(w http.ResponseWriter, r *http.Request) {
-    reqBody, _ := ioutil.ReadAll(r.Body)
-    var message Message
-    json.Unmarshal(reqBody, &message)
-    json.NewEncoder(w).Encode(message)
-    msg := tgbotapi.NewMessage(c.Telegram.ChatID, fmt.Sprintf("%s: %s",message.Sender, message.Content))
-    bot.Send(msg)
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var message Message
+	json.Unmarshal(reqBody, &message)
+	json.NewEncoder(w).Encode(message)
+	msg := tgbotapi.NewMessage(c.Telegram.ChatID, fmt.Sprintf("%s: %s", message.Sender, message.Content))
+	bot.Send(msg)
 }
 
-
 func handleRequests() {
-    myRouter := mux.NewRouter().StrictSlash(true)
-    myRouter.HandleFunc("/send", sendMessage).Methods("POST")
-    log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", c.Server.Port), myRouter))
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/send", sendMessage).Methods("POST")
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", c.Server.Port), myRouter))
 }
 
 func main() {
 
-    c.getConfiguration()
-    bot,err = tgbotapi.NewBotAPI(c.Telegram.Token)
-    if err != nil {
-        log.Panic(err)
-    }
-    handleRequests()
+	c.getConfiguration()
+	bot, err = tgbotapi.NewBotAPI(c.Telegram.Token)
+	if err != nil {
+		log.Panic(err)
+	}
+	handleRequests()
 }
+
 // curl --request POST   --data '{"sender":"curl","content":"Article Content"}'   http://localhost:10000/send
--- tgb.spec
+++ tgb.spec
@@ -1,7 +1,12 @@
 %go_nostrip
 
 %define _buildshell /bin/bash
-%define import_path github.com/bzoltan1/tgb
+%global provider        github
+%global provider_tld    com
+%global project         bzoltan1
+%global repo            tgb
+%global provider_prefix %{provider}.%{provider_tld}/%{project}/%{repo}
+%global import_path     %{provider_prefix}
 
 Name:           tgb
 Version:        0.1
@@ -11,25 +16,25 @@ License:        LGPL-2.1-or-later
 Group:          System/Management
 URL:            https://github.com/bzoltan1/trg
 Source:         %{name}-%{version}.tar.gz
+Source:         vendor.tar.gz
 BuildRequires:  golang-packaging
 
 %{go_nostrip}
 %{go_provides}
 
+%{go_exclusivearch}
+
 %description
 Telegram bridge service for any user or process to send warnings, alerts
 to a configured Telegram user.
 
-
 %prep
-%setup -q -n tgb-%{version}
+%setup -q -n %{repo}-%{version}
 
-# many golang binaries are "vendoring" (bundling) sources, so remove them. Those dependencies need to be packaged independently.
-rm -rf vendor
 
 %build
 %goprep %{import_path}
-%gobuild ...
+%gobuild -mod=vendor "" ...
 
 
 %install
